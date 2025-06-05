ef0003fe0893b44452ead3fd1a0e7824fbaaa5b5
pvblitooo <moralesleiva11@gmail.com>
2025-06-05 00:51:10 -0400

    Codificacion APP

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { IncidentListComponent } from './components/incident-list/incident-list'; 
+import { IncidentFormComponent } from './components/incident-form/incident-form';
+import { StatisticsBoardComponent } from './components/statistics-board/statistics-board';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'incidents', pathMatch: 'full' },
+  { path: 'incidents', component: IncidentListComponent, title: 'Lista de Incidentes' },
+  { path: 'incidents/new', component: IncidentFormComponent, title: 'Nuevo Incidente' }, // Ruta para crear
+  { path: 'incidents/edit/:id', component: IncidentFormComponent, title: 'Editar Incidente' }, // Ruta para editar (el :id es un parámetro)
+  { path: 'statistics', component: StatisticsBoardComponent, title: 'Estadísticas de Incidentes' }, // Ruta añadida
+  { path: '**', redirectTo: 'incidents' }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'; // Importa los módulos de enrutamiento
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'frontend';
+  currentYear = new Date().getFullYear();
 }
