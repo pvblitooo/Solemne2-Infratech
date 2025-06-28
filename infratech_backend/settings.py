223b7d2358fa3ee12067855bb21e36b529a9b4a3
pvblitooo <moralesleiva11@gmail.com>
2025-06-27 21:25:53 -0400

    Configuracion settings

--- infratech_backend/settings.py
+++ infratech_backend/settings.py
@@ -63,10 +63,11 @@ ROOT_URLCONF = 'infratech_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend_build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
