7c25f36e8833bd40088d31bcef4c74a7312b3c6c
pvblitooo <moralesleiva11@gmail.com>
2025-06-27 21:20:53 -0400

    Configuracion ruta backend

--- infratech_backend/urls.py
+++ infratech_backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include # Añadir include
+from django.urls import path, include, re_path # Añadir include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Añadir la ruta para nuestra API
     path('api/', include('incidents.urls')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
